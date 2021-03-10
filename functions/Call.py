2b08c0dd5dba2e42ff1a4bd206a6772f41b1e595
Jean-Charles Isner <charlie068@gmail.com>
2021-03-10 19:48:50 +0000

    added merging file after the analyses

--- functions/Call.py
+++ functions/Call.py
@@ -8,7 +8,8 @@ from collections import OrderedDict
 import glob
 from subprocess import Popen, call
 from timeit import default_timer as timer
-
+import numpy as np
+import re
 # import local modules
 from functions import *
 from PreprocessReads import *
@@ -81,7 +82,7 @@ def Call(config, FUNC_PATH):
 
 
 
-
+    
 
     # get total number of reads
 
@@ -181,7 +182,7 @@ def Call(config, FUNC_PATH):
                 # remove intermediate sam file
                 #for subFile in subFiles:
             #        os.remove(subFile)
-
+                print('\n')
                 PrintTime('console', "MergeSubPileups")
                 # merge sub pileups to obtain whole pileup
                 pileup = MergeSubPileups(pileup,subFiles, OUTPUT)
@@ -286,17 +287,17 @@ def Call(config, FUNC_PATH):
             PrintTime('error', "\t"+message)
 
             PrintTime('console', "\tCalculating statistics...")
-            
+
             # print out stats to console
             message = "Candidate Positions: 0"
             PrintTime('green', "\t\t"+message)
             message = "Final Variants: 0"
             PrintTime('green', "\t\t"+message)
-            
+
             PrintTime('console', "Writing stats file")
             with open(filestats,'a+') as fileout:
                 fileout.write('file {}, No candidate positions were found !\n'.format(bednb))
-                
+
         pileup=None
         full_pileup=None
         potential=None
@@ -306,9 +307,89 @@ def Call(config, FUNC_PATH):
         for subFile in subFiles:
             os.remove(subFile)
         os.remove(SAM)
+
+    #Concatenate files
+    files=glob.glob(OUTPUT+'/*.vcf')
+    #print(files)
+    #print('outputt{}'.format(OUTPUT))
+    file_vcf_out=OUTPUT+"/"+INPUT.replace(".bam", ".vcf").split("/")[-1]
+    #print('fiel vcf out{}'.format(file_vcf_out))
+    with open(file_vcf_out+'interim', "w+") as fvcfout:
+        #parse all vcf files and write in final vcf file
+        for file in files:
+            with open(file, 'r+') as fvcfin:
+                for line in fvcfin:
+                    if line[0]!='#':
+                        fvcfout.write(line)
+            os.remove(file)
+
+    #sort the file
+    with open(file_vcf_out, "w+") as fvcfout:
+        fvcfout.write(GenerateVCFHeader(INPUT, BED, FASTA, OUTPUT, MIN_BASE_QUALITY, \
+                                        MIN_READ_QUALITY, MIN_MAPPING_QUALITY, MIN_VARIANT_UMI, STRAND_BIAS_METHOD, \
+                                        MAX_STRAND_BIAS, CORES, ALPHA, MAX_HP_LENGTH))
+        with open(file_vcf_out+'interim','r+') as fr:
+            lines=sorted((l.split("\t") for l in fr.readlines()), key=lambda l: (int(''.join(filter(lambda i: i.isdigit(),l[0]))),float(l[1])))
+            for line in lines:
+                fvcfout.write('\t'.join(line))
+    #add headers
+    os.remove(file_vcf_out+'interim')
+
+    #concatenate variants
+    file_variants_out=OUTPUT+"/"+INPUT.replace(".bam", ".variants").split("/")[-1]+'.csv'
+    files=glob.glob(OUTPUT+'/*.variants')
+    print(files)
+    with open(file_variants_out+'variants1', "w+") as fvcfout:
+        #parse all vcf files and write in final variants file
+        for file in files:
+            with open(file, 'r+') as fvcfin:
+                lines=fvcfin.readlines()
+                for line in lines[1:]:  #don't count first row
+                        fvcfout.write(line)
+            os.remove(file)
+
+    #sort files
+    header='\t'.join(map(str,["chr","pos","amplicon","reference","idx","A","C","G","T","N","=","+","-",\
+                  "depth","fA","fC","fG","fT","f=","f+","f-","fref","falt","pFisherA",\
+                "pFisherT","pFisherG","pFisherC","pFisherIns","pFisherDel","pFisherA_adjust",\
+                "pFisherT_adjust","pFisherG_adjust","pFisherC_adjust","pFisherIns_adjust",\
+                "pFisherDel_adjust","variantCall","alternative","GR","parsed","n_UMI_wt",\
+                "n_UMI_mt","n_unique_UMI_wt","n_unique_UMI_mt","n_singleton_UMI_wt",\
+                "n_singleton_UMI_mt","n_discordant_UMI_mut","n_concordant_UMI_mut",\
+                "base_error_probability","qScore","p-value","q-value","type","SB","HP",\
+                "confidence","allele_cov_plus", "allele_cov_minus", "coverage_plus", "coverage_minus"]))
+
+    with open(file_variants_out, "w+") as fvcfout:
+        fvcfout.write(header+'\n')
+        with open(file_variants_out+'variants1','r+') as fr:
+            lines=sorted((l.split("\t") for l in fr.readlines()), key=lambda l: (int(''.join(filter(lambda i: i.isdigit(),l[0]))),float(l[1])))
+            for line in lines:
+                fvcfout.write('\t'.join(line))                    
+
+    os.remove(file_variants_out+'variants1')
+    
+    #Sumarise the all stats file
+    #filestats=OUTPUT+"/"+os.path.basename(INPUT).replace(".bam", ".stats")
+    data=[]
+    pattern=re.compile(r": (\d+\.{0,1}\d*)")
+    with open(filestats,'r+') as fin:
+        sample=os.path.basename(file).replace('_sorted.stats','')
+        lines=fin.readlines()
+        for line in lines:
+            data.append(re.findall(pattern,line))
+    Coverage=np.mean([float(x[0]) for x in data])
+    Depth=np.mean([int(x[1]) for x in data])
+    Uniformity=np.mean([float(x[2]) for x in data])
+    Candidate_Positions=sum([int(x[3]) for x in data])
+    Final_Variants=sum([int(x[4]) for x in data])
+    with open(filestats,'a+') as fileout:
+        fileout.write('AVERAGE/TOTAL: BAM/BED average coverage: {}, Average design depth: {}, Average Uniformity: {}, Total Candidate Positions: {}, Total Final Variants: {} \n'.format(Coverage, Depth, Uniformity, Candidate_Positions, Final_Variants))
+
     end = timer()
     etime=time.strftime("%H:%M:%S", time.gmtime(end - start))
     with open(filestats,'a+') as fileout:
                 fileout.write('Elapse time:   {}\n'.format(etime))
+
     PrintTime('console', "Elapse time:   {}".format(etime))
+
 
